3aaa458be7db6ff634b2302b20102bff10b007c6
Redha <redhasouna@yahoo.fr>
2020-03-29 06:01:57 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/zhizhu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.zhizhu;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /** Pelaajien arvot*/
+    public static final byte BLACK_PLAYER = BoardInfo.BLACK,
+            WHITE_PLAYER = BoardInfo.WHITE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.Button_00);
+        btn.setOnClickListener(this);
+
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Button_00:
+                String clicked = "The button is Clicked";
+                        Toast.makeText(this, clicked, Toast.LENGTH_SHORT).show();
+                Button btn = (Button) findViewById(R.id.Button_00);
+                int[] color;
+                color = new int[]{Color.DKGRAY,Color.CYAN,Color.YELLOW,Color.RED,Color.BLUE};
+                int aryLen = color.length;
+                Random random = new Random();
+                int rNum = random.nextInt(aryLen);
+                btn.setBackgroundColor(color[rNum]);
+                break;
+        }
+
+
+    }
+}
